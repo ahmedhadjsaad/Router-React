8ab948a63b73420cfe97698b768d9aad4bfc750c
unknown <ahmed.hadjsaad@iit.ens.tn>
2020-10-06 12:36:17 +0200

    Updates

---
+++ precache-manifest.5f8ad33a3ff1a00121ae8708daf35570.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba89e4ddea0f30581bea2af20e858986",
+    "url": "/Router-React/index.html"
+  },
+  {
+    "revision": "bbd50ee930c351ff1202",
+    "url": "/Router-React/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "2ba3f3df4909c4fb58dc",
+    "url": "/Router-React/static/css/main.b0ffbefc.chunk.css"
+  },
+  {
+    "revision": "bbd50ee930c351ff1202",
+    "url": "/Router-React/static/js/2.8d57150c.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/Router-React/static/js/2.8d57150c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2ba3f3df4909c4fb58dc",
+    "url": "/Router-React/static/js/main.a5d46913.chunk.js"
+  },
+  {
+    "revision": "c898195f82c657392f4a",
+    "url": "/Router-React/static/js/runtime-main.02d841b6.js"
+  }
+]);
